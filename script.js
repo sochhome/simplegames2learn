cbbdce65f6c87e317f05df313d583586c28a1b70
sochhome <70046847+sochhome@users.noreply.github.com>
2023-12-15 11:29:21 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,88 +1,75 @@
 document.addEventListener('DOMContentLoaded', () => {
     const cells = document.querySelectorAll('.cell');
     const statusText = document.getElementById('status');
-    const winningCombinations = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ];
+    const newGameButton = document.getElementById('newGame');
     let currentPlayer = 'X';
     let gameActive = true;
-    let gameState = ['', '', '', '', '', '', '', '', ''];
+    let gameState = Array(9).fill('');
 
-    cells.forEach(cell => {
-        cell.addEventListener('click', cellClick);
-    });
-
-    document.getElementById('newGame').addEventListener('click', newGame);
-
-    function cellClick(clickedCellEvent) {
-        const clickedCell = clickedCellEvent.target;
+    function handleCellClick(e) {
+        const clickedCell = e.target;
         const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
 
         if (gameState[clickedCellIndex] !== '' || !gameActive) {
             return;
         }
 
-        handleCellPlayed(clickedCell, clickedCellIndex);
-        handleResultValidation();
+        updateCell(clickedCell, clickedCellIndex);
+        checkGameOutcome();
     }
 
-    function handleCellPlayed(clickedCell, clickedCellIndex) {
-        gameState[clickedCellIndex] = currentPlayer;
-        clickedCell.classList.add(currentPlayer);
-        clickedCell.textContent = currentPlayer;
+    function updateCell(cell, index) {
+        gameState[index] = currentPlayer;
+        cell.textContent = currentPlayer;
+        cell.classList.add(currentPlayer);
     }
 
-    function handleResultValidation() {
+    function checkGameOutcome() {
+        const winningPatterns = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+            [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+            [0, 4, 8], [2, 4, 6]             // Diagonals
+        ];
+
         let roundWon = false;
-
-        for (let i = 0; i <= 7; i++) {
-            const winCondition = winningCombinations[i];
-            const a = gameState[winCondition[0]];
-            const b = gameState[winCondition[1]];
-            const c = gameState[winCondition[2]];
-
-            if (a === '' || b === '' || c === '') {
-                continue;
-            }
-
-            if (a === b && b === c) {
+        for (let pattern of winningPatterns) {
+            const [a, b, c] = pattern;
+            if (gameState[a] && gameState[a] === gameState[b] && gameState[a] === gameState[c]) {
                 roundWon = true;
                 break;
             }
         }
 
         if (roundWon) {
-            statusText.innerText = `Player ${currentPlayer} Wins!`;
+            statusText.textContent = `Player ${currentPlayer} Wins!`;
             gameActive = false;
             return;
         }
 
-        const roundDraw = !gameState.includes('');
-        if (roundDraw) {
-            statusText.innerText = 'Game Draw!';
+        if (!gameState.includes('')) {
+            statusText.textContent = 'Game Draw!';
             gameActive = false;
             return;
         }
 
         currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-        statusText.innerText = `It's ${currentPlayer}'s turn`;
+        statusText.textContent = `Next Player: ${currentPlayer}`;
     }
 
-    function newGame() {
-        gameActive = true;
-        currentPlayer = 'X';
-        gameState = ['', '', '', '', '', '', '', '', ''];
-        statusText.innerText = `It's ${currentPlayer}'s turn`;
+    function startNewGame() {
+        gameState.fill('');
         cells.forEach(cell => {
             cell.textContent = '';
             cell.classList.remove('X', 'O');
         });
+        currentPlayer = 'X';
+        gameActive = true;
+        statusText.textContent = `Next Player: ${currentPlayer}`;
     }
+
+    cells.forEach(cell => {
+        cell.addEventListener('click', handleCellClick);
+    });
+
+    newGameButton.addEventListener('click', startNewGame);
 });
